4df950a97bc8cebd8618516ee7a0603d4c31b13d
ssj24 <sujee0824@gmail.com>
2019-11-23 12:20:46 +0900

    sociallogin

--- moving/urls.py
+++ moving/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
     path('movies/', include('movies.urls')),
+    # path('oauth/', include('social_django.urls', namespace='social')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL)
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
