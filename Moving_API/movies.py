fc9cdfdb6dd24fc9233bf258ef1eb4e8efddecd5
chajk811 <chajk811@gmail.com>
2019-11-27 20:37:53 +0900

    loaddata_tmp

--- Moving_API/movies.py
+++ Moving_API/movies.py
@@ -3,15 +3,21 @@ import requests
 import json
 from pprint import pprint
 from decouple import config
+import os
+import sys
+import urllib.request
+
 
 movies = []
 result = []
 key = config('KEY')
+client_id = config('CLIENT_ID')
+client_secret = config('CLIENT_SECRET')
 
 # Discover
 # 최신 영화 60개를 result에 담기
 
-for i in range(3):
+for i in range(10):
 # for i in range(1):
     
     page = i+1
@@ -43,7 +49,7 @@ for i in range(len(movies)):
     response_dict = response.json()
 
     title = response_dict["title"]
-    audience = response_dict["popularity"]
+    # audience = response_dict["popularity"]
     start_date = response_dict["release_date"]  
     poster_url = response_dict["poster_path"]
     summary = response_dict["overview"]
@@ -77,8 +83,35 @@ for i in range(len(movies)):
     if response_dict["results"]:
         teaser = response_dict["results"][0]["key"]
     else:
-        teaser = ""
-        
+        continue
+    
+    ## audience
+    base_url_audience = 'https://openapi.naver.com/v1/search/movie.json?query='
+    url_audience = base_url_audience + urllib.parse.quote(f'{title}')
+    
+    request = urllib.request.Request(url_audience)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+
+    response_str = response.read().decode('utf-8')
+    response_dict = json.loads(response_str)
+    
+    if response_dict["total"] != 1:
+        continue
+    elif float(response_dict["items"][0]["userRating"]) == 0.0:
+        continue
+    else:
+        audience = float(response_dict["items"][0]["userRating"])
+
+    # if(rescode==200):
+    #     response_body = response.read()
+    #     print(response_body.decode('utf-8'))
+    # else:
+    #     print("Error Code:" + rescode)
+
+
     movie_tmp['fields'] = {
         'title': title,
         'audience': audience,
